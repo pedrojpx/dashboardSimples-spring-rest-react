92a1285d58b90daea274818eb3636b6059ed4f99
pedrojpx <pjpxavier@gmail.com>
2021-09-12 14:53:11 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
@@ -0,0 +1,33 @@
+package com.devsuperior.dsvendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.entities.Sale;
+import com.devsuperior.dsvendas.repositories.SaleRepository;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)//indica que deve fechar a transação ao finalizar e o readOnly indica que não há escrita prevenindo lock de escrita
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll(); 
+		/**chamado apenas para a JPA trazer os vendedores pro cache e reduzir o número de chamadas ao banco na
+		 * operação seguinte, pois cada Sale tem um Seller. 
+		 * Esta solução traz todos os Sellers para o cache, portanto só atende o caso em que há um número reduzido de Sellers
+		 */
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+}
